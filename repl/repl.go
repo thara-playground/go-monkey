940fc35dd662ff876b42eecf4646a139f4f7ca5d
Tomochika Hara <git@thara.jp>
2018-07-23 23:55:06 +0900

    Ch. 2 : Fix REPL

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/thara/monkey/lexer"
-	"github.com/thara/monkey/token"
+	"github.com/thara/monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -22,9 +22,20 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := sc.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
