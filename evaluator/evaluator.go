08c67fac848dd0601e92b8276fb4b964d04b85c9
Tomochika Hara <git@thara.jp>
2018-08-15 01:10:48 +0900

    Add quote/unquote function for macro

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -66,6 +66,9 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 	case *ast.HashLiteral:
 		return evalHashLiteral(node, env)
 	case *ast.CallExpression:
+		if node.Function.TokenLiteral() == "quote" {
+			return quote(node.Arguments[0], env)
+		}
 		function := Eval(node.Function, env)
 		if isError(function) {
 			return function
